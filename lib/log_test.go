d3346fa0a0149fedd599c59b839e34e37d59ecfd
Geno <genofire@users.noreply.github.com>
2017-03-30 14:40:17 +0200

    Update log_test.go

--- lib/log_test.go
+++ lib/log_test.go
@@ -1,22 +1,22 @@
 package lib
 
 import (
-  "testing"
-
+	"testing"
+	
 	"github.com/stretchr/testify/assert"
- )
+)
 
 func TestLog(t *testing.T) {
-  assertion = assert.New(t)
+	assertion = assert.New(t)
   
-  // No values check, just if it crashed or not
-  LogTimestamp(false)
+	// No values check, just if it crashed or not
+	LogTimestamp(false)
   
-  req, _ := http.NewRequest("GET", "https://google.com/lola/duda?q=wasd")
-  log := Log.GetHTTP(req)
-  _, ok := log.Data["remote"]
+	req, _ := http.NewRequest("GET", "https://google.com/lola/duda?q=wasd")
+	log := LogHTTP(req)
+	_, ok := log.Data["remote"]
   
-  assertion.NotNil(ok, "remote address not set in logger")
-  assertion.Equal("GET", log.Data["method"], "method not set in logger")
-  assertion.Equal("/lola/duda?q=wasd", log.Data["url"], "path not set in logger")
+ 	assertion.NotNil(ok, "remote address not set in logger")
+	assertion.Equal("GET", log.Data["method"], "method not set in logger")
+	assertion.Equal("/lola/duda?q=wasd", log.Data["url"], "path not set in logger")
 }
