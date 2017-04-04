84f060cb3bc8a68b225a22cc9c1d9e6da4be8fc4
Martin Geno <geno+dev@fireorbit.de>
2017-04-04 09:49:46 +0200

    [TASK] add connection to sql log (fix #4)

--- lib/database/main.go
+++ lib/database/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	"github.com/genofire/hs_master-kss-monolith/lib/log"
 )
 
 var (
@@ -17,23 +19,27 @@ type Config struct {
 	Type           string
 	Connection     string
 	ReadConnection string
-	Logging bool
+	Logging        bool
 }
 
 func Open(c Config) (err error) {
+	writeLog := log.Log.WithField("db", "write")
 	config = &c
 	Write, err = gorm.Open(config.Type, config.Connection)
 	Write.SingularTable(true)
 	Write.LogMode(c.Logging)
+	Write.SetLogger(writeLog)
 	Write.Callback().Create().Remove("gorm:update_time_stamp")
 	Write.Callback().Update().Remove("gorm:update_time_stamp")
 	if err != nil {
 		return
 	}
 	if len(config.ReadConnection) > 0 {
+		readLog := log.Log.WithField("db", "read")
 		Read, err = gorm.Open(config.Type, config.ReadConnection)
 		Read.SingularTable(true)
 		Read.LogMode(c.Logging)
+		Read.SetLogger(readLog)
 		Read.Callback().Create().Remove("gorm:update_time_stamp")
 		Read.Callback().Update().Remove("gorm:update_time_stamp")
 	} else {
