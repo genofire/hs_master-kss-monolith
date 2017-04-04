ff299c42a71e7561b6238f7bf3b7bdd14df0fb0e
Martin Geno <geno+dev@fireorbit.de>
2017-04-04 08:18:58 +0200

    [TASK] improve draft of ProductExists

--- models/product.go
+++ models/product.go
@@ -1,6 +1,36 @@
 package models
 
-// TODO DRAFT for a rest request to a other microservice
+import (
+	"net/http"
+	"time"
+)
+
+type boolMicroServiceCache struct {
+	LastCheck time.Time
+	Value     bool
+}
+
+var productExistCache map[int64]boolMicroServiceCache
+
+func init() {
+	productExistCache = make(map[int64]boolMicroServiceCache)
+}
+
 func ProductExists(id int64) (bool, error) {
-	return true, nil
+	if cache, ok := productExistCache[id]; ok {
+		// cache for 5min
+		before := time.Now().Add(-time.Minute * 5)
+		if !cache.LastCheck.Before(before) {
+			return cache.Value, nil
+		}
+	}
+
+	// TODO DRAFT for a rest request to a other microservice
+	res, err := http.Get("http://golang.org")
+
+	productExistCache[id] = boolMicroServiceCache{
+		LastCheck: time.Now(),
+		Value:     (res.StatusCode == http.StatusOK),
+	}
+	return productExistCache[id].Value, err
 }
--- models/product_test.go
+++ models/product_test.go
@@ -8,8 +8,15 @@ import (
 
 func TestProductExists(t *testing.T) {
 	assert := assert.New(t)
-	ok, err := ProductExists(3)
 
+	ok, err := ProductExists(3)
 	assert.True(ok)
 	assert.NoError(err)
+
+	// test cache
+	ok, err = ProductExists(3)
+	assert.True(ok)
+	assert.NoError(err)
+
+	// WARNING: test cache after 5min skipped
 }
