ced8a5d9598f650a15c154a8e07d9ff5011d64b6
Martin Geno <geno+dev@fireorbit.de>
2017-05-12 11:54:59 +0200

    [TASK] add design to add form of goods

--- webroot/static/js/item-add.controller.js
+++ webroot/static/js/item-add.controller.js
@@ -4,6 +4,7 @@ angular.module('microStock')
   .controller('ItemAddCtrl',['$scope','$http','$stateParams',function($scope,$http,$stateParams){
     $scope.product = {};
     $scope.obj = {};
+    $scope.msg = {};
     $scope.count = 1;
 
     $http.get(config.microservice_dependencies.productById.replace("%d", $stateParams.productid)).then(function(res) {
@@ -11,15 +12,20 @@ angular.module('microStock')
     });
 
     $scope.submit = function(){
+      var count = 0;
       function request(){
+        count++;
         return $http.post(config.store.goods.productById.replace("%d",$stateParams.productid),$scope.obj);
       }
       var last = request();
       for(var i=1;i < $scope.count;i++){
         last.then(request);
       }
-      last.then(null,function(){
-        console.log("did not work");
+      last.then(function(){
+        $scope.obj = {};
+        $scope.msg = {type:'success',text:'There was '+count+' goods saved from '+$scope.product.title+'.'};
+      },function(){
+        $scope.msg = {type:'error',text:'There was '+count+' goods saved from '+$scope.product.title+'.'};
       })
     };
   }]);
